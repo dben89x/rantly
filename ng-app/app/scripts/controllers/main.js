5ee0996d904cf715c4bffc19984c7f2881e1bd78
Doug Bennett <dben89x@gmail.com>
2015-03-10 20:52:52 -0600

    meh... idk... tried adding rails views? user resource still not loading

--- ng-app/app/scripts/controllers/main.js
+++ ng-app/app/scripts/controllers/main.js
@@ -1,28 +1,60 @@
-'use strict';
+// 'use strict';
+//
+// /**
+//  * @ngdoc function
+//  * @name rantlyApp.controller:MainCtrl
+//  * @description
+//  * # MainCtrl
+//  * Controller of the rantlyApp
+//  */
+// angular.module('rantlyApp')
+//   .controller('MainCtrl', function ($scope, $resource) {
+//     $scope.awesomeThings = [
+//       'HTML5 Boilerplate',
+//       'AngularJS',
+//       'Karma',
+//     ];
+//
+//     $scope.returnScope = function() {
+//       console.log($resource);
+//     };
+//
+//     $scope.awesomeThings = [
+//       'HTML5 Boilerplate',
+//       'AngularJS',
+//       'Karma'
+//     ];
+//
+//     $scope.addThing = function() {
+//       console.log('poop');
+//       $scope.awesomeThings.push($scope.newThing);
+//       $scope.newThing = {};
+//     };
+//   });
+//
+  'use strict';
 
-/**
- * @ngdoc function
- * @name rantlyApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the rantlyApp
- */
-angular.module('rantlyApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  /**
+   * @ngdoc function
+   * @name rantlyApp.controller:MainCtrl
+   * @description
+   * # MainCtrl
+   * Controller of the rantlyApp
+   */
+  angular.module('rantlyApp')
+    .controller('MainCtrl', function ($scope, $resource) {
+      var User = $resource('/users/:id', {id: '@id'}, {update: {method: 'PUT'}});
+      console.log(User.query());
+      $scope.awesomeThings = User.query();
 
-    $scope.tittyThings = [
-      'poop',
-      'balls',
-      'tits'
-    ];
+      $scope.tittyThings = [
+        'poop',
+        'balls',
+        'tits'
+      ];
 
-    $scope.addThing = function() {
-      $scope.awesomeThings.push($scope.newThing);
-      $scope.newThing = '';
-    };
-  });
+      $scope.addThing = function() {
+        $scope.awesomeThings.push($scope.newThing);
+        $scope.newThing = '';
+      };
+    });
