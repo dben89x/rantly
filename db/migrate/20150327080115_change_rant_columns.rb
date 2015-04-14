7c87f377fd16f512a5745477033c36fedb047dcf
Doug Bennett <dben89x@gmail.com>
2015-04-14 08:09:57 -0600

    removed... weird... migrations...

--- db/migrate/20150327080115_change_rant_columns.rb
+++
@@ -1,6 +0,0 @@
-class ChangeRantColumns < ActiveRecord::Migration
-  def change
-    rename_column :rants, :title, :title
-    rename_column :rants, :body, :body
-  end
-end
--- db/migrate/20150327193739_change_rant_column_names.rb
+++
@@ -1,6 +0,0 @@
-class ChangeRantColumnNames < ActiveRecord::Migration
-  def change
-    rename_column :rants, :title, :title
-    rename_column :rants, :body, :body
-  end
-end
